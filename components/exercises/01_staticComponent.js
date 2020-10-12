e751ae1430f02dbd9ab1293efcf442af001de75d
Wenlong Lu <yummybroccoli@outlook.com>
2020-10-13 00:13:00 +0100

    Exercise 1 completed

--- components/exercises/01_staticComponent.js
+++ components/exercises/01_staticComponent.js
@@ -3,7 +3,30 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>Web App Development 2 - Modules table</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>JavaScript Fundamentals</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>React and Storybook</td>
+            <td>3</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
   );
 };
 
